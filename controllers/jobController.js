a0ec9883aa75ef9b9e56e1bc98da13c0ce65e11f
dbenitez02 <dbenitez101@gmail.com>
2024-01-21 20:57:02 -0800

    Setting up for the search component along with setting up search parameters and setting up pagination

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -7,8 +7,56 @@ import JobModel from "../models/JobModel.js";
 
 /** Get all jobs */
 export const getAllJobs = async (req, res) => {
-    const jobs = await Job.find({ createdBy: req.user.userId });
-    return res.status(StatusCodes.OK).json({ jobs });
+    const { search, jobStatus, jobType, sort } = req.query;
+
+    // Output list of of jobs based on the user logged in.
+    const queryObject = {
+        createdBy: req.user.userId,
+        
+    };
+    
+    // Check if there is anything in the search form.
+    if(search) {
+        queryObject.$or = [
+            // Regex
+            { positon: {$regex: search, $options: 'i'} },
+            { company: {$regex: search, $options: 'i'} },
+        ];
+    }
+
+    if (jobStatus && jobStatus !== 'all') {
+        queryObject.jobStatus = jobStatus;
+    }
+
+    if (jobType && jobType !== 'all') {
+        queryObject.jobType = jobType;
+    }
+
+    // Grouping sorting properties
+    const sortOptions = {
+        newest: '-createdAt',
+        oldest: 'createdAt',
+        'a-z': 'position',
+        'z-a': '-position',
+    };
+
+    // Dynamically select the property
+    const sortKey = sortOptions[sort] || sortOptions.newest;
+
+    // Setting up pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    // Sort jobs when a sortKey is defined
+    const jobs = await Job.find(queryObject).sort(sortKey).skip(skip).limit(limit);
+
+    // Getting total number of jobs
+    const totalJobs = await Job.countDocuments(queryObject);
+
+    // Getting total number of pages
+    const numOfPages = Math.ceil(totalJobs / limit);
+    return res.status(StatusCodes.OK).json({ totalJobs, numOfPages, currentPage:page, jobs });
 };
 
 /** Create a Job */
